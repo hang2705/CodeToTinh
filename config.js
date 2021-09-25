fca5b0f1859373465fde9ffe6078945204dc39d2
hang2705 <59534665+hang2705@users.noreply.github.com>
2021-09-25 17:40:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+const CONFIG = {
+  introTitle: "Hee luu cậu",
+  introDesc: `Tôi có điều này muốn hỏi cậu, cậu phải trả lời thật lòng nha ♥♥♥☺☺`,
+  btnIntro: "Hãy nhấn Oke nhé",
+  title: "Cậu yêu tớ có phải không?",
+  desc: "Cậu không trả lời hoặc thoát ra, thì cậu là người yêu tôi đấy nhá ",
+  btnYes: "Đồng ý",
+  btnNo: "Cậu mơ à :))",
+  btnSuyNnghi: "Để suy nghĩ",
+  question: "Nói cho tớ lý do vì sao cậu yêu tớ?",
+  reply: "Tại vì bạn dễ thương, hiểu chuyện và rất là vui tính <3333",
+  btnReply: "Gửi cho tớ ♥♥",
+  mess: "Tôi biết mà ☺. Love you 3000 ♥♥♥",
+  messDesc: "Còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào!",
+  btnAccept: "Yêu ơi là yêu <333",
+  messLink: "https://www.facebook.com/profile.php?id=100009200704499", //link fb
+  bgLink: "https://huypham9205.github.io/tang-crush/img/bg.webp", //link ảnh nền
+  //   iputBgLink: "../img/HINH(990).jpg", // ảnh background cho ảnh
+  iputBgLink: "https://huypham9205.github.io/tang-crush/img/iput-bg.jpg", //link iputBgLink
+  lookMeLink: "https://huypham9205.github.io/tang-crush/img/lookMe.jpg", //link ảnh lookMe
+  // lookMeLink: "../HINH(1093).jpg", //hình của mình bỏ vào
+
+  soundLink: "https://huypham9205.github.io/tang-crush/sound/sound.mp3", //link nhac
+  duckLink: "https://huypham9205.github.io/tang-crush/sound/duck.mp3", //link am thanh 1
+  swishLink: "https://huypham9205.github.io/tang-crush/sound/Swish1.mp3", //link swish
+};
